74ddc1692374c4ebd15a1a7ca1eadaaf324674c4
adaotilia <159614421+adaotilia@users.noreply.github.com>
2025-05-22 22:44:15 +0200

    "update"

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,12 @@
 import adapter from '@sveltejs/adapter-netlify';
-export default {
-  kit: {
-    adapter: adapter()
-  }
-}
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	},
+	preprocess: vitePreprocess()
+};
+
+export default config;
